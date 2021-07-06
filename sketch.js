1d08a11ee34834ffa11a9de66379a4f80c5394ea
Ayan-345 <86412781+Ayan-345@users.noreply.github.com>
2021-07-06 10:53:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var path;
+var pathIMG;
+var runner;
+var runnerIMG;
+
+function preload(){
+  //pre-load images
+pathIMG = loadImage("path.png");
+runnerIMG = loadAnimation("runner-1.png","runner-1.png","runner-2.png","runner-1.png")
+
+}
+
+function setup(){
+  createCanvas(400,400);
+
+  //create sprites here 
+
+  path = createSprite(200,200);
+  path.addImage(pathIMG);
+  path.velocityY = 4;
+  path.scale = 1.2;
+
+ runner = createSprite(200,300)
+ runner.addAnimation("jacksonrunning",runnerIMG)
+ runner.scale = 0.1
+runner.velocityY = -0.08
+
+invisibleGround1 = createSprite(50,350,10,400);
+  invisibleGround1.visible = false;
+
+  invisibleGround2 = createSprite(350,350,10,400);
+  invisibleGround2.visible = false;
+
+
+}
+
+function draw() {
+  background(0);
+  path.velocityY = 4;
+
+  if ( path.y > 400){
+    path.y = height/5
+  }
+  drawSprites()
+  if (keyDown("right")) {
+    runner.x = runner.x + 3;
+  }
+  if (keyDown("left")) {
+   runner.x = runner.x - 3;
+  }
+runner.collide(invisibleGround1);
+runner.collide(invisibleGround2);
+
+
+
+}
